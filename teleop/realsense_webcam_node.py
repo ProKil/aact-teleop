47d672129b54990d967b46bc000031119601dd5b
Su Li <suli1921129@gmail.com>
2024-07-12 10:56:37 -0400

    fixing precommit issues

--- teleop/realsense_webcam_node.py
+++ teleop/realsense_webcam_node.py
@@ -1,16 +1,14 @@
 import asyncio
-from logging import getLogger
 import time
-from typing import Any, AsyncIterator, Self
 
 import cv2
-from pubsub_server import Node, NodeFactory, Message
-from pubsub_server.messages import Tick, Image
+from pubsub_server import NodeFactory
 from teleop.webcam_node import WebcamNode
 
 import numpy as np
 
 from stretch_body.hello_utils import setup_realsense_camera  # type: ignore
+
 
 @NodeFactory.register("realsense")
 class RealsenseWebcamNode(WebcamNode):
@@ -23,7 +21,6 @@ class RealsenseWebcamNode(WebcamNode):
     ):
         super().__init__(input_tick_channel, output_channel, webcam_id, redis_url)
 
-
     async def update_video_feed(self) -> None:
         """
         Use a different process to update the video feed.
@@ -31,18 +28,17 @@ class RealsenseWebcamNode(WebcamNode):
 
         self.logger.debug("Starting video feed.")
 
-        #serial number first input 
+        # serial number first input
         pipeline = setup_realsense_camera(None, (1280, 720), (1280, 720), 30)
 
-
-        while not self.shutdown_event.is_set():            
+        while not self.shutdown_event.is_set():
             start_time = time.time()
             frames = pipeline.wait_for_frames()
             color_frame = frames.get_color_frame()
             if not color_frame:
                 continue
 
-            color_image = np.rot90(np.asanyarray(color_frame.get_data()), -1)   
+            color_image = np.rot90(np.asanyarray(color_frame.get_data()), -1)
 
             ret, buffer = cv2.imencode(".jpg", color_image)
             self.latest_frame = buffer.tobytes()
