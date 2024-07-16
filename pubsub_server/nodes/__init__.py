6125484a027efa0631b1c82917eeac220095d575
Hao Zhu <prokilchu@gmail.com>
2024-07-16 16:16:10 -0400

    Speech recognition and text-to-speech (#32)
    * add transcriber
    
    * add transcriber and tts nodes

--- pubsub_server/nodes/__init__.py
+++ pubsub_server/nodes/__init__.py
@@ -4,7 +4,10 @@ from .random import RandomNode
 from .record import RecordNode
 from .listener import ListenerNode
 from .speaker import SpeakerNode
+from .transcriber import TranscriberNode
 from .performance import PerformanceMeasureNode
+from .print import PrintNode
+from .tts import TTSNode
 from .registry import NodeFactory
 
 __all__ = [
@@ -15,5 +18,8 @@ __all__ = [
     "RecordNode",
     "ListenerNode",
     "SpeakerNode",
+    "TranscriberNode",
     "PerformanceMeasureNode",
+    "PrintNode",
+    "TTSNode",
 ]
